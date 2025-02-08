4748ca41b83ac54a55064b5af6ebb94108602473
the invictus codes <hannanhacks@gmail.com>
2025-02-08 23:04:05 +0530

    p17 added

---
+++ usePortfolioCard.js
@@ -0,0 +1,60 @@
+import i1 from "../assets/images/portfoliowebsite/p11.png";
+import i2 from "../assets/images/portfoliowebsite/p12.png";
+import i3 from "../assets/images/portfoliowebsite/p13.png";
+import i4 from "../assets/images/portfoliowebsite/p14.png";
+import i5 from "../assets/images/portfoliowebsite/p15.png";
+import i7 from "../assets/images/portfoliowebsite/p17.png";
+
+export const projects = [
+  {
+    title: "Portfolio Webiste p17",
+    image: i7,
+    technologies: [" React▪️", "Tailwind CSS"],
+    liveUrl: "https://alisha17.vercel.app/",
+    githubUrl: "https://github.com/codesalisha/Portfolio_17",
+    cost: "free",
+  },
+  {
+    title: "Portfolio Webiste p11",
+    image: i1,
+    technologies: [" React▪️", "Tailwind CSS▪️", "framer-motion"],
+    liveUrl: "https://alisha11.vercel.app/",
+    githubUrl: "https://github.com/codesalisha/Portfolio_11",
+    cost: "free",
+  },
+
+  {
+    title: "Portfolio Webiste p12",
+    image: i2,
+    technologies: [" React▪️", "Tailwind CSS▪️", "framer motion"],
+    liveUrl: "https://alisha12.vercel.app/",
+    githubUrl: "https://github.com/codesalisha/Portfolio_12",
+    cost: "free",
+  },
+
+  {
+    title: "Portfolio Webiste p13",
+    image: i3,
+    technologies: [" React▪️", "Tailwind CSS"],
+    liveUrl: "https://alisha13.vercel.app/",
+    githubUrl: "https://github.com/codesalisha/Portfolio_13",
+    cost: "free",
+  },
+  {
+    title: "Portfolio Webiste p14",
+    image: i4,
+    technologies: [" React▪️", "Tailwind CSS"],
+    liveUrl: "https://alisha14.vercel.app/",
+    githubUrl: "https://github.com/codesalisha/Portfolio_14",
+    cost: "free",
+  },
+
+  {
+    title: "Portfolio Webiste p15",
+    image: i5,
+    technologies: [" React▪️", "Tailwind CSS"],
+    liveUrl: "https://alisha15.vercel.app/",
+    githubUrl: "https://github.com/codesalisha/Portfolio_15",
+    cost: "free",
+  },
+];
